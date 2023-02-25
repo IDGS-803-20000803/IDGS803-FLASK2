8ebe6dda3ca90f7e2941f292d6fb387e370a883d
Daniel Abraham Sanchez Aboytes <daniel.abraham.sanchez.aboytes@gmail.com>
2023-02-24 19:03:05 -0600

    Actividad1.24/02/2023

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import request
 import forms
+from collections import Counter
 app =Flask(__name__)
 
 @app.route("/formulario")
@@ -24,13 +25,34 @@ def form():
 @app.route("/resultado", methods = ['GET', 'POST'])
 def process():
     if request.method == 'POST':
-        fields = int(request.form.get('txtNum'))
-
-        return render_template('Dinamicas.html', fields = fields)
-    elif request.method == 'GET':
-        return render_template('') 
+        numberFields = int(request.form.get('txtNum'))
+        return render_template('Dinamicas.html', numberFields = numberFields)
     
-    return render_template('Principal.html')
+
+
+@app.route("/calcular", methods = ['GET', 'POST'])
+def calcular():
+  
+  numbersString = request.form.getlist('txtNum')
+  numbersInt = list(map(int, numbersString))
+
+  major = max(numbersInt)
+  minor = min(numbersInt)
+
+  average = sum(numbersInt) / len(numbersInt)
+
+  for i in range(len(numbersString)):
+        numbersString[i] = int(numbersString[i])
+
+  counter = Counter(numbersString)
+  resultados = counter.most_common()
+  results = []
+  for r in resultados:
+        if r[1] > 1:
+            results.append('El numero {0} se repite {1}'.format(r[0], r[1]))
+ 
+
+  return render_template('resultado.html', major = major, minor = minor, average = average, results = results)
 
 if __name__ == "__main__":
     app.run(debug=True)
