753ae75711d70196bad87abc4320ac2bf205dc91
Daniel Abraham Sanchez Aboytes <daniel.abraham.sanchez.aboytes@gmail.com>
2023-03-03 17:22:19 -0600

    Actividad 2 y 3.DanielAbrahamSanchezAboytes.03/03/2023

--- app.py
+++ app.py
@@ -4,21 +4,19 @@ from flask_wtf.csrf import CSRFProtect
 from collections import Counter
 from flask import make_response
 from flask import flash
+from config import Config
 import forms
-import diccionario
-import actividad2
-
-
+from forms import GuardarForm, BuscarForm,UseForm,LoginForm,ResistorForm
 
 app =Flask(__name__)
-'''
+
 app.config['SECRET_KEY'] = "Esta es la clave encriptada"
 csrf = CSRFProtect()
-'''
+
 @app.route('/Diccionario', methods=['GET', 'POST'])
 def index():
-    formGuardar = actividad2.GuardarForm(request.form)
-    formBuscar = actividad2.BuscarForm(request.form)
+    formGuardar = GuardarForm(request.form)
+    formBuscar = BuscarForm(request.form)
     result = ''
 
     # Si se presiona el boton de guardar
@@ -30,8 +28,11 @@ def index():
             # Guarda la informacion en el archivo de texto
             with open('diccionario.txt', 'a') as file:
                 file.write(espanolP + '=' + inglesP + '\n')
+        else:
+            danger_message = 'Es necesario llenar ambas entradas para guardar un registro'
+            flash(danger_message)       
     if request.method == 'POST' and 'limpiar' in request.form:
-       return render_template('diccionario.html', formGuardar=formGuardar, formBuscar=formBuscar, result=result)
+       return render_template('actividad2.html', formGuardar=formGuardar, formBuscar=formBuscar, result=result)
     # Si se presiona el boton de buscar
     if request.method == 'POST' and 'buscar' in request.form:
         if formBuscar.validate():
@@ -49,11 +50,69 @@ def index():
                         result = spanish
                         break
                 else:
+                    danger_message = 'Palabra no encontrada'
+                    flash(danger_message) 
                     result = 'Palabra no encontrada'
     if request.method == 'POST' and 'limpiar' in request.form:
        return render_template('actividad2.html', formGuardar=formGuardar, formBuscar=formBuscar, result=result)
     return render_template('actividad2.html', formGuardar=formGuardar, formBuscar=formBuscar, result=result)
 
+@app.route('/resis', methods=['GET', 'POST'])
+def calRes():
+    formRes = forms.ResistorForm(request.form)
+    colorBanda1 =""
+    colorBanda2 =""
+    hexaMultip = ""
+    colorTolerancia =""
+    valorResistencia =""
+    max_valor =""
+    min_valor =""
+    hexaBanda1 =""
+    hexaBanda2 =""
+    hexaTolerancia =""
+
+    if request.method == 'POST' and formRes.validate():
+   
+        colorBanda1 = formRes.band1.data
+        colorBanda2 = formRes.band2.data
+        colorMultiplicador = formRes.multiplier.data
+        colorTolerancia = formRes.tolerance.data
+      
+        hexaBanda1 = Config.BAND_COLORS[colorBanda1]
+        hexaBanda2 = Config.BAND_COLORS[colorBanda2]
+        hexaTolerancia =Config.TOLERANCE_COLORS[colorTolerancia]
+        hexaMultip = Config.MULTIPLICADOR[colorMultiplicador]
+
+
+        valorResistencia = (int(colorBanda1 + colorBanda2) * 10**int(colorMultiplicador))
+
+
+        valorNominal = int(colorBanda1 + colorBanda2) * 10**int(colorMultiplicador)
+
+        if colorTolerancia == 'oro':
+            valorTolerancia = 0.05
+        elif colorTolerancia == 'plata':
+            valorTolerancia = 0.1
+        else:
+            valorTolerancia = Config.TOLERANCE_VALUES[colorTolerancia]
+
+        max_valor = valorNominal * (1 + valorTolerancia)
+        min_valor = valorNominal * (1 - valorTolerancia)
+        
+        success_message='¡El calculo se ha realizado con exito!, El valor de la resistencia es {}Ω'.format(valorResistencia)
+        flash(success_message)
+
+        #return response
+        
+    response = make_response(render_template('resistencia.html', formRes=formRes, band1_color=colorBanda1, band2_color=colorBanda2, 
+                        multiplier_color=hexaMultip, tolerance_color=colorTolerancia, 
+                        resistance_value=valorResistencia, resistance_max=max_valor, resistance_min=min_valor,
+                        band1_name=hexaBanda1, band2_name=hexaBanda2, tolerance_name=hexaTolerancia))
+
+    response.set_cookie('resistance_value', str(valorResistencia))
+    response.set_cookie('resistance_max', str(max_valor))
+    response.set_cookie('resistance_min', str(min_valor))
+    return response
 
 @app.errorhandler(404)
 def no_encontrada(e):
@@ -69,7 +128,7 @@ def cookies():
         datos = user + '@'+passw
         succes_message = 'Bienvenido {}'.format(user)
         flash(succes_message)
-    response=make_response(render_template('cookies.html', form = reg_user))
+    response = make_response(render_template('cookies.html', form = reg_user))
     if len(datos)> 0:
         response.set_cookie('datos_user',datos)
     return response
@@ -131,7 +190,6 @@ def calcular():
   return render_template('resultado.html', major = major, minor = minor, average = average, results = results)
 
 if __name__ == "__main__":
-    '''
+    
     csrf.init_app(app)
-    '''
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from wtforms import Form
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList, FormField,SelectField, RadioField
+from wtforms import StringField, SubmitField, FieldList, FormField,SelectField, RadioField,validators
 from wtforms.fields import EmailField 
 from wtforms import validators
+
 
 def mi_validacion(form,field):
     if len(field.data) == 0:
@@ -19,6 +20,42 @@ class LoginForm(Form):
                                          validators.length(min=4,max=15, message="No cumple la longitud para el campo")])
     password = StringField('password',[validators.data_required(message="El campo es requerido")])
 
+# Define la clase del formulario para la seccion de guardar
+class GuardarForm(Form):
+    espanol = StringField('Espanol', [
+        validators.DataRequired(message='Es necesario ingresar una palabra en espanol'),
+        validators.InputRequired()])
+    ingles = StringField('Ingles', [ 
+        validators.DataRequired(message='Es necesario ingresar una palabra en ingles'),
+         validators.InputRequired()])
 
+# Define la clase del formulario para la seccion de busqueda
+class BuscarForm(Form):
+    palabra_buscar = StringField('Palabra', [ 
+        validators.DataRequired(message='Es necesario ingresar una palabra para realizar la busqueda'),
+        validators.InputRequired()])
+    respuesta = RadioField('Idioma de busqueda', choices=[('es', 'Espanol'), ('en', 'Ingles')],
+        validators=[validators.InputRequired(message='Seleccione una opcion')])
 
-
+class ResistorForm(Form):
+    colors = [('', 'Seleccione una Opcion'),
+              ('0', 'Negro'), 
+              ('1', 'Marron'),
+              ('2', 'Rojo'),
+              ('3', 'Naranja'), 
+              ('4', 'Amarillo'), 
+              ('5', 'Verde'),
+              ('6', 'Azul'),
+              ('7', 'Violeta'), 
+              ('8', 'Gris'), 
+              ('9', 'Blanco')]
+    band1 = SelectField('Banda 1', choices=colors, validators=[validators.InputRequired('Elija una opcion correcta')])
+    band2 = SelectField('Banda 2', choices=colors, validators=[validators.InputRequired('Elija una opcion correcta')])
+    multiplier = SelectField('Multiplicador', choices=[('0', 'Negro x1 Ω'), ('1', 'Marron x10 Ω'), 
+                                              ('2', 'Rojo x100 Ω'), ('3', 'Naranja x1 kΩ'),
+                                              ('4', 'Amarillo x10 kΩ'), ('5', 'Verde x100 kΩ'), 
+                                              ('6', 'Azul x1 MΩ'), ('7', 'Violeta x10 MΩ'),
+                                              ('8', 'Gris x100 MΩ'), ('9', 'Blanco x10 GΩ')],
+                                                       validators=[validators.InputRequired()])
+    tolerance = RadioField('Tolerancia', choices=[('oro', 'ORO = 5%'), ('plata', 'PLATA = 10%')], 
+                           validators=[validators.InputRequired(message='Este campo no puede quedarse vacio')])                        
